7761040e4935d2b60509ed83cedb5673867dde0a
JojoMarques <tavares.marques@aluno.ifsp.edu.br>
2024-09-04 15:21:12 -0300

    update

--- JavaTokio2024/src/logica/exercicios/aula06/Exercicio05.java
+++ JavaTokio2024/src/logica/exercicios/aula06/Exercicio05.java
@@ -12,13 +12,13 @@ public class Exercicio05 {
 		System.out.println("entre com um numero:");
 		a = scan.nextFloat();
 		
-		System.out.println("entre com outro numero:");
-		b = scan.nextFloat();
-		
 		scan.nextLine();
 		
 		System.out.println("entre com um operador matematico (+, -, *, /)");
 		char operador = scan.nextLine().charAt(0);
+		
+		System.out.println("entre com outro numero:");
+		b = scan.nextFloat();
 		
 		switch(operador) {
 			case '+':
@@ -34,8 +34,12 @@ public class Exercicio05 {
 				System.out.println("multiplicacao = "+resultado);
 				break;
 			case '/':
-				resultado = a/b;
-				System.out.println("divisao = "+resultado);
+				if(b != 0) {
+					resultado = a/b;
+					System.out.println("divisao = "+resultado);
+				}
+				else
+					System.out.println("operacao invalida");
 				break;
 				
 			default:
