b4f37ac778843285a3954e3d60f4165081ade49f
Jossana Tavares Marques <126785824+JojoMarques@users.noreply.github.com>
2024-09-10 10:59:11 -0300

    Update Exercicio02.java

--- JavaTokio2024/src/logica/exercicios/aula08/Exercicio02.java
+++ JavaTokio2024/src/logica/exercicios/aula08/Exercicio02.java
@@ -1,1 +1,15 @@
+package logica.exercicios.aula08;
 
+import java.util.Calendar;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+    
+		Calendar calendar = Calendar.getInstance();
+    
+		if(calendar.get(Calendar.DAY_OF_WEEK) == 3)
+			System.out.println("hoje é terça-feira, tem feira de rua.");
+	}
+
+}
