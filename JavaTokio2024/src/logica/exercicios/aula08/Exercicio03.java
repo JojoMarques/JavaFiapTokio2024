8108b320f440fcdc5368001e66e5d91eed25402c
Jossana Tavares Marques <126785824+JojoMarques@users.noreply.github.com>
2024-09-10 11:10:27 -0300

    Update Exercicio03.java

--- JavaTokio2024/src/logica/exercicios/aula08/Exercicio03.java
+++ JavaTokio2024/src/logica/exercicios/aula08/Exercicio03.java
@@ -14,27 +14,24 @@ public class Exercicio03 {
 		int ctmaius = 0,ctminus = 0 ,ctnum = 0 ,vazio = 0;
 
 		for (int i = 0; i < palavra.length(); i++) {
-      //maiúsculo
+      			//maiúsculo
 			if ((palavra.charAt(i) >= 'A') && (palavra.charAt(i) <= 'Z')) 
 				ctmaius++;
-        // minúsculo
-			else 
-        if ((palavra.charAt(i) >= 'a') && (palavra.charAt(i) <= 'z')) 
+       			 // minúsculo
+			else if ((palavra.charAt(i) >= 'a') && (palavra.charAt(i) <= 'z')) 
 				ctminus++;
-          // numérico
-			else 
-          if ((palavra.charAt(i) >= '0') && (palavra.charAt(i) <= '9'))
+          		// numérico
+			else if ((palavra.charAt(i) >= '0') && (palavra.charAt(i) <= '9'))
 		      ctnum++;
-            // vazio
-			else 
-            if (palavra.charAt(i) == ' '){
+            		// vazio
+			else if (palavra.charAt(i) == ' '){
 				    vazio++;
 		}
       
-			System.out.println("quantidade de caracteres alfabéticos maiúsculos: "+ctmaius);
-			System.out.println("quantidade de caracteres alfabéticos minúsculos: "+ctminus);
-			System.out.println("quantidade de caracteres numéricos: "+ctnum);
-			System.out.println("quantidade de espaço em branco: "+vazio);
+		System.out.println("quantidade de caracteres alfabéticos maiúsculos: "+ctmaius);
+		System.out.println("quantidade de caracteres alfabéticos minúsculos: "+ctminus);
+		System.out.println("quantidade de caracteres numéricos: "+ctnum);			
+		System.out.println("quantidade de espaço em branco: "+vazio);
 
       scan.close();
 	}
