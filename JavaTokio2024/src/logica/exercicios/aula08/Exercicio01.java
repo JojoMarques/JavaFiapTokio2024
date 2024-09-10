7145ec5494f65e3c25b4de51f47ba3c1fa861cdf
Jossana Tavares Marques <126785824+JojoMarques@users.noreply.github.com>
2024-09-10 10:53:30 -0300

    Update Exercicio01.java
    update exercise 01

--- JavaTokio2024/src/logica/exercicios/aula08/Exercicio01.java
+++ JavaTokio2024/src/logica/exercicios/aula08/Exercicio01.java
@@ -1,9 +1,27 @@
 package logica.exercicios.aula08;
+import java.util.Scanner;
+import java.util.Calendar;
 
 public class Exercicio01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner (system.in);
+
+		String nome;
+		int hora;
+		
+		System.out.println("entre com seu nome:");
+		nome = scan.nextLine();
+
+		Calendar calendar = Calendar.getInstance();
+		hora = calendar.get(Calendar.HOUR_OF_DAY);
+
+		if(hora >=12 && hora<12)
+			System.out.println("bom dia, "+nome);
+		if(hora>12 && hora<17)
+			System.out.println("boa tarde, "+nome);
+		else
+			System.out.println("boa noite, "+nome);
 
 	}
 
